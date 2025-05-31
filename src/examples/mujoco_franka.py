246cbf723fd5e037f08a812855b90cf72a23566d
David Dorf <daviddorf2023@u.northwestern.edu>
2025-05-31 11:08:50 -0400

    Closes elegantly with ctrl c now

--- src/examples/mujoco_franka.py
+++ src/examples/mujoco_franka.py
@@ -8,10 +8,13 @@ pixi_root = Path(os.environ['PIXI_PROJECT_ROOT'])
 model = mujoco.MjModel.from_xml_path(str(pixi_root / "src/marionette/models" / "franka_research_3/scene.xml"))
 data = mujoco.MjData(model)
 with mujoco.viewer.launch_passive(model, data) as viewer:
-  while viewer.is_running():
-    print("position", data.qpos)
-    print("control", data.ctrl)
-    print("velocity", data.qvel)
-    print("acceleration", data.qacc)
-    mujoco.mj_step(model, data)
-    viewer.sync()
+    while viewer.is_running():
+        try:
+            # print("position", data.qpos)
+            # print("control", data.ctrl)
+            # print("velocity", data.qvel)
+            # print("acceleration", data.qacc)
+            mujoco.mj_step(model, data)
+            viewer.sync()
+        except KeyboardInterrupt:
+            break
