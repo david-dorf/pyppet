0c10a8663678567104dac04d1b042d96b1c07872
David Dorf <daviddorf2023@u.northwestern.edu>
2025-05-30 21:24:11 -0400

    use passive viewer and get position, control, velocity, accel from mujoco. keyboard interrupt works with context manager added with passive viewer

--- src/examples/mujoco_franka.py
+++ src/examples/mujoco_franka.py
@@ -7,4 +7,11 @@ from pathlib import Path
 pixi_root = Path(os.environ['PIXI_PROJECT_ROOT'])
 model = mujoco.MjModel.from_xml_path(str(pixi_root / "src/marionette/models" / "franka_research_3/scene.xml"))
 data = mujoco.MjData(model)
-mujoco.viewer.launch(model, data)
+with mujoco.viewer.launch_passive(model, data) as viewer:
+  while viewer.is_running():
+    print("position", data.qpos)
+    print("control", data.ctrl)
+    print("velocity", data.qvel)
+    print("acceleration", data.qacc)
+    mujoco.mj_step(model, data)
+    viewer.sync()
